d08a6f720474459dd5edda7c54d45a86a1365a0a
Michael Fich <michaeltf@gmail.com>
2015-06-10 14:41:41 -0400

    Exercise 5: Wrote the encapsulated code for the fahrenheit_to_celsius method.  Returns temperature in celsius.

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,7 @@
 # defines a method for converting temperature from fahrenheit to celsius
 def fahrenheit_to_celsius(fahrenheit)
-
+	# formula to convert fahrenheit to celsius
+	(fahrenheit - 32) * (5.to_f / 9.to_f)
 end
 
 # prints a message requesting that the user input the current temperature
@@ -9,4 +10,3 @@ print ">> "
 
 # collects the input from the user and saves it as fahrenheit
 fahrenheit = gets.strip
-
