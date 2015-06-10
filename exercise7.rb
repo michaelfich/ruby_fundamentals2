7df21496496896cd9918142e6f8cbf219facc85e
Michael Fich <michaeltf@gmail.com>
2015-06-10 15:52:35 -0400

    Exercise 7: Created a method to display each cohort in a hash and the number of students per cohort.

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,12 @@
+# set up a method to display the students in each cohort
+def display_cohorts(cohorts)
+	# iterate over the hash of cohorts
+	cohorts.each do |cohort, students|
+		# outputs each cohort and the number of students within it
+		puts "#{cohort}: #{students}"
+	end
+end
+
 # sets a hash containing the number of students per cohort
 students = {
   :cohort1 => 34,
@@ -5,3 +14,4 @@ students = {
   :cohort3 => 22
 }
 
+display_cohorts(students)
