13f60a8f4ce4963968f82b38c4ffd2eabfa8c8fb
Michael Fich <michaeltf@gmail.com>
2015-06-10 14:53:59 -0400

    Exercise 5: Called the fahrenheit_to_celsius conversion method to convert the user input to Fahrenheit, and outputs the results to the user.

--- exercise5.rb
+++ exercise5.rb
@@ -3,7 +3,7 @@ def fahrenheit_to_celsius(fahrenheit)
 	# convert the parameter 'fahrenheit' to an integer
 	fahrenheit = fahrenheit.to_i
 
-	# formula to convert fahrenheit to celsius
+	# converts fahrenheit to celsius and returns the value as a float
 	(fahrenheit - 32) * (5.to_f / 9.to_f)
 end
 
@@ -13,3 +13,12 @@ print ">> "
 
 # collects the input from the user and saves it as fahrenheit
 fahrenheit = gets.strip
+
+# calls the fahrenheit_to_celsius method to convert the temperature to celsius
+celsius = fahrenheit_to_celsius(fahrenheit)
+
+# rounds the temperature in celsius to two precision points
+celsius.round(2)
+
+# outputs the converted temperature to user
+puts "#{fahrenheit}° Fahrenheit is equal to #{celsius}° celsius"
