d21f53ef6fec90e328b7bdae54d59e01a4da34c5
Michael Fich <michaeltf@gmail.com>
2015-06-10 14:30:16 -0400

    Exercise 5: Prompts the user to enter the temperature in Fahrenheit.

---
+++ exercise5.rb
@@ -0,0 +1,7 @@
+# prints a message requesting that the user input the current temperature
+puts "Enter the temperature in Fahrenheit:"
+print ">> "
+
+# collects the input from the user and saves it as fahrenheit
+fahrenheit = gets.strip
+
