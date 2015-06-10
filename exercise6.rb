6b7c568422d7e766ffaf56aa780fcf1317791aa6
Michael Fich <michaeltf@gmail.com>
2015-06-10 15:03:10 -0400

    Exercise 6: Moved the code to display a list of groceries into a new method, named display_grocery_list.  It accepts an array as a parameter.

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,13 @@
+# created a method to output the grocery list
+def display_grocery_list(groceries)
+	# iterate over the grocery list and output each item
+	groceries.each do |grocery|
+		puts "* #{grocery}"
+	end
+end
+
 # initialize the grocery_list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |grocery|
-	puts "* #{grocery}"
-end
+# display the grocery list
+display_grocery_list(grocery_list)
